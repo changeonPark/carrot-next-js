4e082bc44ce5534874ae2efafe1c4fd0c7ec98ea
parkchangeon <cgp@altava.com>
2022-07-11 18:51:01 +0900

    isLiked 추가

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,50 +7,63 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) => {
-  const { id } = req.query
-  console.log("id", id)
-  if (id) {
-    const product = await client.product.findUnique({
-      where: {
-        id: +id.toString(),
+  const {
+    query: { id },
+    session: { user },
+  } = req
+
+  if (!id || !user) return res.status(404).json({ ok: false })
+
+  const product = await client.product.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
       },
-      include: {
-        user: {
-          select: {
-            id: true,
-            name: true,
-            avatar: true,
+    },
+  })
+
+  const terms = product?.name.split(" ").map(word => ({
+    name: {
+      contains: word,
+    },
+  }))
+
+  let relatedProducts = null
+
+  if (terms) {
+    relatedProducts = await client.product.findMany({
+      where: {
+        OR: terms[0],
+        AND: {
+          id: {
+            not: product?.id,
           },
         },
       },
     })
-
-    const terms = product?.name.split(" ").map(word => ({
-      name: {
-        contains: word,
-      },
-    }))
-
-    let relatedProducts = null
-
-    if (terms) {
-      relatedProducts = await client.product.findMany({
-        where: {
-          OR: terms[0],
-          AND: {
-            id: {
-              not: product?.id,
-            },
-          },
-        },
-      })
-      console.log("relatedProducts: ", relatedProducts)
-    }
-
-    res.json({ ok: true, product, relatedProducts })
-  } else {
-    console.log("else")
+    console.log("relatedProducts: ", relatedProducts)
   }
+
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: {
+        productId: product?.id,
+        userId: user.id,
+      },
+      select: {
+        id: true,
+      },
+    })
+  )
+
+  res.json({ ok: true, product, isLiked, relatedProducts })
 }
 
 export default withApiSession(withHandler({ methods: ["GET"], handler }))
