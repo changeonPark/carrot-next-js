9534cba384cc8ab80c9757f46a3b8a858eaf4767
parkchangeon <cgp@altava.com>
2022-08-07 22:45:49 +0900

    middleware redirect & rewrite + check Bot

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,37 @@
 /* eslint-disable @next/next/no-server-import-in-page */
-import { NextRequest, userAgent } from "next/server"
+import { NextRequest, userAgent, NextResponse } from "next/server"
 
 export function middleware(request: NextRequest) {
-  if (
-    request.nextUrl.pathname.startsWith("/chats") ||
-    request.nextUrl.pathname.startsWith("/chats/:path*")
-  ) {
-    console.log("chats Only middleware")
-    const url = request.nextUrl
-    console.log("url: ", url)
-    const user = userAgent(request)
-    console.log("device \n", user)
-  }
+  if (!request.nextUrl.pathname.startsWith("/_next/static")) {
+    // const { isBot } = userAgent(request)
+    // if (isBot) {
+    //   // https://nextjs.org/docs/messages/returning-response-body-in-middleware
+    //   // return new Response("Fucking Bot!, 껒여", { status: 403 })
+    // }
 
-  console.log("it Works at global!")
+    if (!request.nextUrl.pathname.includes("/api")) {
+      console.log("pathname: ", request.nextUrl.pathname)
+      if (
+        !request.cookies.get("carrot") &&
+        // !request.nextUrl.pathname.includes("/enter")
+        request.nextUrl.pathname !== "/enter"
+      ) {
+        // return NextResponse.rewrite(new URL("/enter", request.url))
+        const loginUrl = new URL("/enter", request.url)
+        // loginUrl.searchParams.set("from", request.nextUrl.pathname)
+        return NextResponse.redirect(loginUrl)
+      }
+    }
+
+    if (
+      request.nextUrl.pathname.startsWith("/chats") ||
+      request.nextUrl.pathname.startsWith("/chats/:path*")
+    ) {
+      console.log("chats Only middleware")
+      const url = request.nextUrl
+      console.log("url: ", url)
+    }
+
+    console.log("it Works at global!")
+  }
 }
